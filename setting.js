b5539d3b12cb72b93b84b5bd373812442ce8bf0b
zhuweilog <zhuweilong1127@qq.com>
2023-03-15 18:00:42 +0800

    安装第三方组件库，配置eslint和prettier

---
+++ setting.js
@@ -0,0 +1,4 @@
+// 这里要用commitapi
+module.exports = {
+  title: '默认系统'
+}
---
+++ src/views/login.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="login-wrapper">
+    <div class="header">登录</div>
+    <div class="form-wrapper">
+      <a-input></a-input>
+      <a-input></a-input>
+      <a-button></a-button>
+    </div>
+    <div class="msg">
+      Don't have account?
+      <a href="#">Sign up</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+  background-image: linear-gradient(to right, #fbc2eb, #a6c1ee);
+}
+.login-wrapper {
+  background-color: #fff;
+  width: 358px;
+  height: 588px;
+  border-radius: 15px;
+  padding: 0 50px;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+.header {
+  font-size: 38px;
+  font-weight: bold;
+  text-align: center;
+  line-height: 200px;
+}
+.input-item {
+  display: block;
+  width: 100%;
+  margin-bottom: 20px;
+  border: 0;
+  padding: 10px;
+  border-bottom: 1px solid rgb(128, 125, 125);
+  font-size: 15px;
+  outline: none;
+}
+.input-item:placeholder {
+  text-transform: uppercase;
+}
+.btn {
+  text-align: center;
+  padding: 10px;
+  width: 100%;
+  margin-top: 40px;
+  background-image: linear-gradient(to right, #a6c1ee, #fbc2eb);
+  color: #fff;
+}
+.msg {
+  text-align: center;
+  line-height: 88px;
+}
+a {
+  text-decoration-line: none;
+  color: #abc1ee;
+}
+</style>
